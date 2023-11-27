3085d71db2f5923027734f1f79bae1ac0cd00e3a
MichaelDHuy <dachuy87@gmail.com>
2023-11-27 10:28:08 -0800

    Add systray, real life update and open image functions

--- static/src/dashboard.js
+++ static/src/dashboard.js
@@ -1,5 +1,4 @@
 /** @odoo-module **/
-
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { getDefaultConfig } from "@web/views/view";
@@ -9,75 +8,63 @@ import { Card } from "./card/card";
 import { PieChart } from "./pie_chart/pie_chart";
 import { sprintf } from "@web/core/utils/strings";
 
-const { Component, useSubEnv, onWillStart } = owl;
+const { Component, useSubEnv, useState } = owl;
 
 class AwesomeDashboard extends Component {
   setup() {
-     // The useSubEnv below can be deleted if you're > 16.0
+    // The useSubEnv below can be deleted if you're > 16.0
     useSubEnv({
       config: {
-          ...getDefaultConfig(),
-          ...this.env.config,
+        ...getDefaultConfig(),
+        ...this.env.config,
       },
     });
-  
     this.display = {
       controlPanel: { "top-right": false, "bottom-right": false },
     };
 
     this.action = useService("action");
-    this.tshirtService = useService("tshirtService");
+    const tshirtService = useService("tshirtService");
+    this.statistics = useState(tshirtService.statistics);
 
     this.keyToString = {
       average_quantity: this.env._t("Average amount of t-shirt by order this month"),
-      average_time: this.env._t(
-          "Average time for an order to go from 'new' to 'sent' or 'cancelled'"
-      ),
+      average_time: this.env._t("Average time for an order to go from 'new' to 'sent' or 'cancelled'"),
       nb_cancelled_orders: this.env._t("Number of cancelled orders this month"),
       nb_new_orders: this.env._t("Number of new orders this month"),
       total_amount: this.env._t("Total amount of new orders this month"),
     };
-    onWillStart(async () => {
-      this.statistics = await this.tshirtService.loadStatistics();
-    });
   }
 
   openCustomerView() {
     this.action.doAction("base.action_partner_form");
   }
-
   openOrders(title, domain) {
     this.action.doAction({
-        type: "ir.actions.act_window",
-        name: title,
-        res_model: "awesome_tshirt.order",
-        domain: new Domain(domain).toList(),
-        views: [
-            [false, "list"],
-            [false, "form"],
-        ],
+      type: "ir.actions.act_window",
+      name: title,
+      res_model: "awesome_tshirt.order",
+      domain: new Domain(domain).toList(),
+      views: [
+        [false, "list"],
+        [false, "form"],
+      ],
     });
   }
   openLast7DaysOrders() {
-    const domain =
-        "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d'))]";
+    const domain = "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d'))]";
     this.openOrders(this.env._t("Last 7 days orders", domain));
   }
-
   openLast7DaysCancelledOrders() {
-    const domain =
-        "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')), ('state','=', 'cancelled')]";
+    const domain = "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')), ('state','=', 'cancelled')]";
     this.openOrders(this.env._t("Last 7 days cancelled orders"), domain);
   }
-  
   openFilteredBySizeOrders(size) {
     const title = sprintf(this.env._t("Filtered orders by %s size"), size);
     const domain = `[('size','=', '${size}')]`;
     this.openOrders(title, domain);
   }
 }
-
 AwesomeDashboard.components = { Layout, Card, PieChart };
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
-
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
